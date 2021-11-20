a9bfdde1fd4eac5a7ecd4719187ab014263cea4d
danighnatios <93642585+danighnatios@users.noreply.github.com>
2021-11-20 16:44:09 +0200

    Our Wedding - Update 1

--- js/main.js
+++ js/main.js
@@ -277,28 +277,36 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-  let birthday = "Dec 15, 2021 00:00:00",
-      countDown = new Date(birthday).getTime(),
+		
+  let wedding_Date = "Dec 26, 2021 14:00:00",
+  
+  
+      countDown = new Date(wedding_Date).getTime(),
       x = setInterval(function() {    
         let now = new Date().getTime(),
             distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+   
+        document.getElementById("days").innerText = Math.floor(distance / (day)) ,
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
         //do something later when date is reached
+		
         if (distance < 0) {
-          let headline = document.getElementById("headline"),
-              countdown = document.getElementById("countdown"),
-              content = document.getElementById("content");
+			
+		document.getElementById("...").innerText = "It's our wedding!";
+        let headline = document.getElementById("headline"),
+        countdown = document.getElementById("countdown"),
+        content = document.getElementById("content");
 
           headline.innerText = "It's our wedding!";
           countdown.style.display = "none";
           content.style.display = "block";
-
           clearInterval(x);
+		  
+		  
+		  
         }
         //seconds
       }, 0)
